279b27c115882c602556281e3db5aa55f2eaad4d
vladimirven001 <vladimir.venkov@mail.mcgill.ca>
2023-03-08 18:23:48 -0500

    MyList

--- src/assignment2/MyDoublyLinkedList.java
+++ src/assignment2/MyDoublyLinkedList.java
@@ -13,6 +13,39 @@ public class MyDoublyLinkedList<E> extends MyLinkedList<E> {
 	 * ADD YOUR CODE HERE
 	 */
 
+	public boolean add(E element) {
+		DNode added = new DNode();
+		if (this.tail != null) {
+			added.element = element;
+			added.next = null;
+			added.prev = this.tail;
+			this.tail.next = added;
+			this.tail = added;
+		}
+		else {
+			added.element = element;
+			this.head = added;
+			this.tail = this.head;
+		}
+		return true;
+	}
+
+	public E remove() {
+		if (this.isEmpty()) {
+			throw new NoSuchElementException("The linked list is empty.");
+		}
+		else {
+			E element = this.tail.element;
+			(this.tail.prev).next = null;
+			this.tail = this.tail.prev;
+			return element;
+		}
+	}
+
+	public boolean addFirst() {
+
+	}
+
 	private class DNode {
 		private E element;
 		private DNode next;
--- src/assignment2/MyLinkedList.java
+++ src/assignment2/MyLinkedList.java
@@ -1,1 +1,20 @@
 package assignment2;
+
+public abstract class MyLinkedList<E> implements MyList<E> {
+    protected int size;
+
+    public MyLinkedList() {
+        size = 0;
+    }
+    public boolean isEmpty() {
+        if (this.size == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public int getSize() {
+        return this.size;
+    }
+}
